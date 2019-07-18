37598474d079d659e1684da46a62dd6c73248367
TienNDSE62860 <44231091+TienNDSE62860@users.noreply.github.com>
2019-07-18 11:28:35 +0700

    create notify error screen

---
+++ README.md
@@ -0,0 +1,2 @@
+# RepairGrab
+HCI201
---
+++ app/src/main/java/com/example/repairgrab/activity/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.repairgrab.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+import com.example.repairgrab.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Spinner dropdownlist;
+    private EditText edt_username;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edt_username = findViewById(R.id.edt_username);
+
+    }
+
+
+
+
+    public void clickToLogin(View view) {
+        String username = edt_username.getText().toString().trim();
+        if (username.equals("duytien")) {
+            Intent intent = new Intent(this, RepairerHomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
